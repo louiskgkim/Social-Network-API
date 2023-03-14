ebb0ff791815bc770c73cf2a182955e78196a432
louiskgkim <lkgk31@gmail.com>
2023-03-14 10:06:23 -0700

    changed file names and started working on the controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,9 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+
+  getThought(req, res) {
+    Thought.find({})
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+const { User, Thought } = require("../models");
+
+module.exports = {
+  getUser(req, res) {
+    User.find({})
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
