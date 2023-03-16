e510428300e6f5d46d11cae1772064130c2da575
louiskgkim <lkgk31@gmail.com>
2023-03-15 20:26:48 -0700

    getting a template down for thought controllers

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,9 +1,32 @@
 const { User, Thought } = require("../models");
 
 module.exports = {
-
   getThought(req, res) {
     Thought.find({})
       .then((thought) => res.json(thought))
       .catch((err) => res.status(500).json(err));
   },
+
+  getSingleThought(req, res) {
+    
+  },
+
+  createThought(req, res) {
+    
+  },
+
+  updateThought(req, res) {
+    
+  },
+
+  deleteThought(req, res) {
+    
+  },
+
+  createReaction(req, res) {
+    
+  },
+
+  deleteReaction(req, res) {
+    
+};
